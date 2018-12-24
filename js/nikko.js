4465ca33912633414aa188f8a99838f16627a2a7
mhbi1 <michaelhanbi@gmail.com>
2018-12-24 16:44:15 -0700

    minor changes to background

--- js/nikko.js
+++ js/nikko.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
       $(".backToTop").show()
   } else {
        $(".backToTop").hide();
