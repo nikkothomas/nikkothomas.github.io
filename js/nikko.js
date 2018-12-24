5ca8081b2050ecb572e0a21b1c866337a0689b15
mhbi1 <michaelhanbi@gmail.com>
2018-12-24 16:48:40 -0700

    more minor changes

--- js/nikko.js
+++ js/nikko.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
       $(".backToTop").show()
   } else {
        $(".backToTop").hide();
